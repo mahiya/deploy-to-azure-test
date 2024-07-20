90457ef4dd20c32a9d58b3623b8efbe84add6ffc
Masayuki Hiyama <hiiyan0402@gmail.com>
2024-07-20 11:37:10 +0900

    update

---
+++ assets/script2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+curl -o search_index.json https://raw.githubusercontent.com/mahiya/deploy-to-azure-test/main/assets/search_index.json
+
+curl -X PUT https://$searchServiceName.search.windows.net/indexes/$searchServiceIndexName?api-version=2024-05-01-preview \
+    -H 'Content-Type: application/json' \
+    -H 'api-key: '$searchServiceApiKey \
+    -d "$(sed -e "s|{{VECTORIZER_RESOURCE_URI}}|$openAIServiceAccountEndpoint|; \
+                  s|{{VECTORIZER_API_KEY}}|$openAIServiceAccountKey|; \
+                  s|{{VECTORIZER_DEPLOYMENT_ID}}|$openAIServiceEmbeddingsDeployName|;" \
+                  "search_index.json")"
